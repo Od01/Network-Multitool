88da7940a816a6d4ad7079ae2b7b1b2eea6d07e1
Od01 <det.john.kimball.2@gmail.com>
2017-04-08 17:10:13 -0400

    Adding crawler_prod

---
+++ crawler/crawler_prod.py
@@ -0,0 +1,25 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from scrapy.item import Item
+from scrapy.spider import BaseSpider
+
+class hsleidenSpider(CrawlSpider):
+        name = "hsleiden1"
+        allowed_domains = ["docs.python.org"]
+        start_urls = ["https://docs.python.org/2/library/functions.html"]
+
+        # allow=() is used to match all links
+        rules = [
+        Rule(SgmlLinkExtractor(allow=()), follow=True, callback='parse_item')
+        #Rule(SgmlLinkExtractor(allow=()), callback='parse_item'), # Added this to first rule
+        ]
+
+        def parse_item(self, response):
+            x = HtmlXPathSelector(response)
+            filename = "hsleiden-output.txt"
+            open(filename, 'w')
+            for resp in x:
+                write(resp.url + "\n")
+
+                # got from http://stackoverflow.com/questions/13740825/trying-to-crawl-all-links-of-a-webpage-with-scrapy-but-i-cannot-output-the-link
