8d1db19fa466a0a198c34ef9f638a8c91a392b02
Od01 <det.john.kimball.2@gmail.com>
2017-04-08 20:41:59 -0400

    changed from iterable

--- crawler/crawler_prod.py
+++ crawler/crawler_prod.py
@@ -6,8 +6,8 @@ from scrapy.spider import BaseSpider
 
 class hsleidenSpider(CrawlSpider):
         name = "hsleiden1"
-        allowed_domains = ["docs.python.org"]
-        start_urls = ["https://docs.python.org/2/library/functions.html"]
+        allowed_domains = ["quotes.toscrape.com"]
+        start_urls = ["http://quotes.toscrape.com/"]
 
         # allow=() is used to match all links
         rules = [
@@ -18,8 +18,9 @@ class hsleidenSpider(CrawlSpider):
         def parse_item(self, response):
             x = HtmlXPathSelector(response)
             filename = "hsleiden-output.txt"
-            open(filename, 'w')
-            for resp in x:
-                write(resp.url + "\n")
+            open(filename, 'w').write(response.url + "\n")
+            #file = open(filename, "w")
+            #for resp in x:
+                #file.write(resp.url + "\n")
 
                 # got from http://stackoverflow.com/questions/13740825/trying-to-crawl-all-links-of-a-webpage-with-scrapy-but-i-cannot-output-the-link
