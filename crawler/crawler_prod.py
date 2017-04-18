afc4f561c8fe2f5aee16d9a162c54d58bf2a3418
Od01 <det.john.kimball.2@gmail.com>
2017-04-17 21:22:17 -0400

    added edit

--- crawler/crawler_prod.py
+++ crawler/crawler_prod.py
@@ -5,6 +5,7 @@ from scrapy.item import Item
 from scrapy.spiders import BaseSpider
 from scrapy import Request
 from scrapy.http import Request
+from scrapy.utils.httpobj import urlparse
 #from run_first import *
 
 class InputSpider(CrawlSpider):
@@ -14,8 +15,13 @@ class InputSpider(CrawlSpider):
         #def allowed_domains(self):
             #self.allowed_domains = user_input
 
-        def start_requests(self):
-            yield Request(url=self.user_input)
+        #def start_requests(self):
+            #yield Request(url=self.user_input)
+
+        def __init__(self, *args, **kwargs):
+            inputs = kwargs.get('urls', '').split(',') or []
+            self.allowed_domains = [urlparse(d).netloc for d in inputs]
+            self.start_urls = [urlparse(c).netloc for c in inputs]
 
         rules = [
         Rule(SgmlLinkExtractor(allow=()), follow=True, callback='parse_item')
