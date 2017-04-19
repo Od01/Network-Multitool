ab8fd5b0af23baf704f14893de7a8712377cbfff
Od01 <det.john.kimball.2@gmail.com>
2017-04-18 23:10:51 -0400

    Edit run_first

--- crawler/run_first.py
+++ crawler/run_first.py
@@ -2,7 +2,8 @@ import os
 
 def userInput():
     user_input = raw_input("Please enter URL. Please do not include http://: ")
-    allowed_domains = user_input
+
+
     os.system("scrapy runspider -a url=" + user_input + " crawler_prod.py")
 
 userInput()
