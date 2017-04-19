568158e6724eb44c86e2c1b2942fd490d20ef52e
Od01 <det.john.kimball.2@gmail.com>
2017-04-19 18:21:31 -0400

    Removed unneeded code

--- crawler/crawler_prod.py
+++ crawler/crawler_prod.py
@@ -6,24 +6,9 @@ from scrapy.spiders import BaseSpider
 from scrapy import Request
 from scrapy.http import Request
 from scrapy.utils.httpobj import urlparse
-#from run_first import *
 
 class InputSpider(CrawlSpider):
         name = "Input"
-        #allowed_domains = ["example.com"]
-
-        #def allowed_domains(self):
-            #self.allowed_domains = user_input
-
-        #def start_requests(self):
-            #yield Request(url=self.user_input)
-
-# Stack Overflow edits
-        #def __init__(self, *args, **kwargs):
-            #inputs = kwargs.get('urls', '').split(',') or []
-            #self.allowed_domains = [urlparse(d).netloc for d in inputs]
-            # self.start_urls = [urlparse(c).netloc for c in inputs] # For start_urls
-# End Stack Overflow edits
 
         def __init__(self, url=None, *args, **kwargs):
             super(InputSpider, self).__init__(*args, **kwargs)
