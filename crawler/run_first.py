4d02ab20dc1e37a8110f79df48b334e37b0fdda3
Od01 <det.john.kimball.2@gmail.com>
2017-04-20 20:56:21 -0400

    Added string testing

--- crawler/run_first.py
+++ crawler/run_first.py
@@ -1,8 +1,17 @@
 import os
+import sys
 
 def userInput():
     user_input = raw_input("Please enter URL. Please do not include http://: ")
+    # forbidden = "http://"
 
+    def checkString(forbidden, string, error_message):
+        if forbidden in string:
+            user_input = None
+            sys.exit(error_message)
+
+    checkString("http://", user_input, "Please don't include http://. Try again")
+    checkString("https://", user_input, "Please don't include https://. Try again")
 
     os.system("scrapy runspider -a url=" + user_input + " crawler_prod.py")
 
