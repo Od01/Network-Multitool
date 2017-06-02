14216ef11cce00619b9292d2281caca0bad66b92
Od01 <det.john.kimball.2@gmail.com>
2017-06-01 22:18:20 -0400

    started scanner

--- Directory_Scanner/scanner.py
+++ Directory_Scanner/scanner.py
@@ -1,1 +1,15 @@
-import
+from urllib2 import urlopen
+
+link = urlopen('http://georgeoffley.com')
+
+statuses = [
+    404, # Not found
+    200, # success
+    403 #Forbidden
+
+    ]
+
+status = link.getcode()
+
+if status == statuses[1]:
+    print "The site exists"
