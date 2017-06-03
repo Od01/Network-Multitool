2b3c02052e1145260d0809a84387cf3fc7941555
Od01 <det.john.kimball.2@gmail.com>
2017-06-03 18:39:05 -0400

    Created check function

--- Directory_Scanner/scanner.py
+++ Directory_Scanner/scanner.py
@@ -1,15 +1,27 @@
 from urllib2 import urlopen
 
-link = urlopen('http://georgeoffley.com')
+url = raw_input("Please type in a valud URL. Do not include the leading 'http://'")
 
-statuses = [
-    404, # Not found
-    200, # success
-    403 #Forbidden
+link = urlopen("http://" + url)
 
-    ]
+def checkUrl(link):
+    statuses = [
+        200, # success
+        404, # Not found
+        403, #Forbidden
+        401, # Unauthorized
+        500, # Internal server erro
+        503, # Service Unavailable
+        504 # Gateway Timeout
 
-status = link.getcode()
+        ]
 
-if status == statuses[1]:
-    print "The site exists"
+    status = link.getcode()
+
+    if status == statuses[0]:
+        global stat
+        stat = 1
+checkUrl(link)
+
+if stat == 1:
+    print "Good to move forward"
