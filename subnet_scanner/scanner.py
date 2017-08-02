4be888a063d044b5abe60b5f204720b5d4a8c766
Od01 <det.john.kimball.2@gmail.com>
2017-08-01 21:07:27 -0400

    Added some testing

--- subnet_scanner/scanner.py
+++ subnet_scanner/scanner.py
@@ -1,9 +1,16 @@
 import socket
+import sys
 
 net_addr =  raw_input("Please put in network address, without a node address. Example: 192.168.0 ")
+last_string = net_addr[-1:]
+
+if last_string == ".":
+	print "Please enter a network address with the format '192.168.0' please don't include the . at the end."
+	sys.exit()
 
 for addr in range(2,255):
 	str(net_addr)
+
 	try:
 		#str(addr)
 		print socket.gethostbyaddr(net_addr + "." + str(addr))
