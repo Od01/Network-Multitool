77c02a10697117e142d81f6c277a4bc36d341510
Od01 <det.john.kimball.2@gmail.com>
2017-08-01 22:28:31 -0400

    Working on subnet scanner

--- subnet_scanner/scanner.py
+++ subnet_scanner/scanner.py
@@ -1,18 +1,23 @@
 import socket
 import sys
+import os
 
 net_addr =  raw_input("Please put in network address, without a node address. Example: 192.168.0 ")
 last_string = net_addr[-1:]
+
+# print socket.gethostbyaddr("10.0.2.15")
 
 if last_string == ".":
 	print "Please enter a network address with the format '192.168.0' please don't include the . at the end."
 	sys.exit()
 
 for addr in range(2,255):
-	str(net_addr)
-
 	try:
-		#str(addr)
 		print socket.gethostbyaddr(net_addr + "." + str(addr))
+		print net_addr + "." + str(addr) + " is alive"
+		#host_up = True if os.system("ping -c 1 " + net_addr + "." + str(addr)) is 0 else False
+		#if host_up == True:
+			#print net_addr + "." + addr + " is alive"
 	except socket.error:
-		print(net_addr + "." + str(addr) + " Not found")
+		#print(net_addr + "." + str(addr) + " Not found")
+		print "no"
