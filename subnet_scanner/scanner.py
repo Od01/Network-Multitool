afc4522f124058a3ab0371ec521361ce511178a6
Od01 <det.john.kimball.2@gmail.com>
2017-08-12 22:31:23 -0400

    Added threading

--- subnet_scanner/scanner.py
+++ subnet_scanner/scanner.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import os
+from threading import Thread
 
 net_addr =  raw_input("Please put in network address, without a node address. Example: 192.168.000 or 010.001.056\n")
 last_string = net_addr[-1:]
@@ -13,20 +14,28 @@ if length_string != 11:
 	print "Please enter a network address with the format '192.168.000' or 010.001.056 please don't include the . at the end."
 	sys.exit()
 
-for addr in range(2,255):
-	# Combine address entered and range number
-	ip = net_addr + "." + str(addr)
+def addr_checker(net_addr):
+	for addr in range(2,255):
+		# Combine address entered and range number
+		ip = net_addr + "." + str(addr)
 
-	# Validate IP address format
-	try:
-		socket.inet_aton(ip)
-	except socket.error:
-		print "Not a valid IP address, closing scanner."
-		sys.exit()
+		# Validate IP address format
+		try:
+			socket.inet_aton(ip)
+		except socket.error:
+			print "Not a valid IP address, closing scanner."
+			sys.exit()
 
-	# Check the address
-	try:
-		print socket.gethostbyaddr(ip)
-		print ip + " is alive"
-	except socket.error:
-		print "Nothing is alive at " + ip
+		# Check the address
+		try:
+			print socket.gethostbyaddr(ip)
+			print ip + " is alive"
+		except socket.error:
+			print "Nothing is alive at " + ip
+
+		size = 20
+		for i in range(1, 1000, size):
+			t = Thread(target=addr_checker, args=(i, i+size))
+			t.start
+
+addr_checker(net_addr)
