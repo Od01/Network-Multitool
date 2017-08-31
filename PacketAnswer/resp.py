b3cbcb0d318f06a6a9134bfb76e203e9b5f43348
Od01 <det.john.kimball.2@gmail.com>
2017-08-30 22:20:06 -0400

    Header update

--- PacketAnswer/resp.py
+++ PacketAnswer/resp.py
@@ -2,12 +2,14 @@ import requests
 
 site = raw_input("Please enter website url to check. Do not include 'http://'\n")
 
+
 def webServerInfo(site):
+	headers = {'content-type': 'Server'}
 	try:
-		r = requests.get('https://' + site, verify=True)
+		r = requests.get('https://' + site, verify=True, headers=headers)
 	except:
 		print "There was a connection error"
 
-	print(r.headers)
+	print r
 
 webServerInfo(site)
