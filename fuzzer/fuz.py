30b112c766e76c361d40b8ac70dd3354c98e9f03
Od01 <det.john.kimball.2@gmail.com>
2017-08-31 23:01:12 -0400

    url fuzzer

---
+++ fuzzer/fuz.py
@@ -0,0 +1,11 @@
+import requests
+
+url = raw_input("Please type in a URL to look through without the leading URL\n")
+found_urls = []
+numbers = '0123456789'
+alnum = numbers + 'abcdefghijklmnopqrstuvwxyz'
+
+ for i in range(0,100):
+	 for i in range(0,100):
+
+	 	r = requests.get("https://" + url + /)
